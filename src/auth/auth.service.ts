347e8932e3f1be6fb4b8d35e9cd31ef4b0dca0db
vfa-nhiny <110367409+vfa-nhiny@users.noreply.github.com>
2022-10-30 15:36:25 +0700

    fix: content of email forgotPassword

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -263,8 +263,6 @@ export class AuthService {
           'Hi! <br><br> If you requested to reset your password<br><br>' +
           '<a href=' +
           config.host.url +
-          ':' +
-          config.host.port +
           '/auth/email/reset-password/' +
           tokenModel.newPasswordToken +
           '> Click here </a>', // html body
